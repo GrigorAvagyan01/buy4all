0685a556e1d61bf43597e82a513bf7833568062e
GrigorAvagyan01 <grigoravagyan482@gmail.com>
2025-05-28 09:08:58 +0400

    buy4all

---
+++ app/src/main/java/com/grish/buy4all4/RulesActivity.java
@@ -0,0 +1,24 @@
+package com.grish.buy4all4;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RulesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Ad Posting Rules");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
